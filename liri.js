7b45252beb091d6ade39bb095b5b2c0105b98526
Shivam Parekh <Shivam@Shivam-Parekhs-MacBook.local>
2016-12-14 17:13:32 -0500

    Keys/Access Tokens

---
+++ liri.js
@@ -0,0 +1,15 @@
+// Importing Twitter API keys
+
+var twitterKeys = require('keys.js');
+
+// Request node packages
+
+var Twitter = require('twitter');
+
+var Spotify = require('spotify');
+
+var request = require('request');
+
+// fs library
+
+var fs = require('fs');
